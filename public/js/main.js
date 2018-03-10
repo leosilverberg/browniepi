cb5c019914edd39dee068db7559ad036ab404c89
leosilverberg <leosilverberg@gmail.com>
2018-03-10 22:49:58 +0000

    setup for pi zero

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ var socket = io();
 socket.on("init", function(msg){
     console.log(msg);
     msg.forEach(msg => {
-        var split = msg.split("\\");
+        var split = msg.split("/");
         console.log(split);
         $("#photo-container").append("<div class='pure-u-1-4'> <img src='photos/"+split[2]+"'class='pure-img'/>");
     });
