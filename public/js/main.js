9b91735af7c2e288ee22fa9b99cdc968be5dcdc9
Leo Silverberg <leosilverberg@gmail.com>
2018-03-11 14:23:31 +0000

    stylin

--- public/js/main.js
+++ public/js/main.js
@@ -5,12 +5,12 @@ socket.on("init", function(msg){
     msg.forEach(msg => {
         var split = msg.split("/");
         console.log(split);
-        $("#photo-container").append("<div class='pure-u-1-1 photo'> <img src='photos/"+split[2]+"'class='pure-img'/>");
+        $("#photo-container").append("<div class='pure-u-1-1 photo-holder'> <img src='photos/"+split[2]+"'class='pure-img'/>");
     });
 });
 
 socket.on("newphoto", function(msg){
-    $("#photo-container").append("<div class='pure-u-1-1 photo'> <img src='photos/"+msg+"'class='pure-img'/>");
+    $("#photo-container").append("<div class='pure-u-1-1 photo-holder'> <img src='photos/"+msg+"'class='pure-img'/>");
 })
 
 $('#snap-button').click(function(){
